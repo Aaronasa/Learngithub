d95265f3ca0e70f0bb095dc4580de43a44477d40
drphnd <darrenpandiangan2322@gmail.com>
2024-10-22 11:57:14 +0700

    Update AcelFanBase.kt

---
+++ app/src/main/java/com/example/github/AcelFanBase.kt
@@ -0,0 +1,14 @@
+package com.example.github
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun baru(){
+    Text("Halo Update baru dari branch rafi")
+    Text("Halo Update baru dari branch rafi")
+    Text("Hahaha HIHIHI di edit acel")
+    Text(
+        "MANTANcantengannnnn"
+    )
+}

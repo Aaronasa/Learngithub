8d9f7344d6a62af4526ea0b83f5329e06d619f1a
drphnd <darrenpandiangan2322@gmail.com>
2024-10-22 12:04:34 +0700

    jkfsala

--- app/src/main/java/com/example/github/Cantengan.kt
+++ app/src/main/java/com/example/github/Cantengan.kt
@@ -1,8 +1,10 @@
 package com.example.github
 
 import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
 
-fun Cantengan{
+@Composable
+fun Cantengan(){
     Text(
         "Acel Cantengan"
     )

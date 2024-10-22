62571db11c04e0de3a08599ef6c21627cdf1cc5b
marcellino Indra Wijaya <marcellinowijaya39@gmail.com>
2024-10-22 11:26:44 +0700

    sepuh acel nih bos

---
+++ app/src/main/java/com/example/github/AcelFanBase.kt
@@ -0,0 +1,9 @@
+package com.example.github
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun baru(){
+    Text("Halo Update baru dari branch rafi")
+}
